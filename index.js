47badba5db73b579b64d513476176497b8874b23
Steven Roper <sroper@uheaa.ushe.local>
2016-12-05 16:37:53 -0700

    Connect to MongoDB.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
