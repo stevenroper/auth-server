ee7cd7cbde6be36f8789ab6deab4aa2d3a8914bc
Steven Roper <sroper@uheaa.ushe.local>
2016-12-06 09:13:27 -0700

    Create basic signup API.

---
+++ controllers/authentication.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: "You must provide an email and a password."})
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }
+
+    if (existingUser) {
+      return res.status(422).send({ error: "There is already an account using the provided email."});
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      res.json({ success: true });
+    });
+  });
+}
--- router.js
+++ router.js
@@ -1,9 +1,5 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app) {
-  app.get('/', function(req, res, next) {
-    res.send({
-      name: 'Steven',
-      company: 'STG',
-      experience: 'ehh....'
-    });
-  });
+  app.post('/signup', Authentication.signup);
 }
